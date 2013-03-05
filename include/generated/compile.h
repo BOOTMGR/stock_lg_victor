9414e0cf7b01169d9eee0c7718bb4f735cc32419
nomanman <harsh.dude16@gmail.com>
2013-03-05 13:27:37 +0530

    Some missing files

---
+++ include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 1 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 PREEMPT Tue Mar 5 13:19:17 IST 2013"
+#define LINUX_COMPILE_TIME "13:19:17"
+#define LINUX_COMPILE_BY "harsh"
+#define LINUX_COMPILE_HOST "MS-7528"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.7.2 20120731 (prerelease) (Linaro GCC 4.7-2012.08) "
